20bd6ac338ab2362ceb5d31399ba01c7da783d23
Christopher David <chris@arcade.city>
2018-09-25 15:01:37 -0500

    Cleanup (#24)

--- desktop/src/App.js
+++ desktop/src/App.js
@@ -1,6 +1,7 @@
 import { Provider } from 'mobx-react'
 import * as React from 'react'
 import './config'
+import './config/reactotron'
 // import './App.css'
 import { setupRootStore } from './setup/setup-root-store'
 import { TestUnlockScreen } from './components/TestUnlockScreen'
