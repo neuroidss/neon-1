7bca1353c0f7091cd3773ab4df506244095ef46d
Christopher David <chris@arcade.city>
2018-09-26 14:15:38 -0500

    Add lerna reset scripts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native'
+import { Button } from '@arcadecity/neon-ui'
 
 export default class App extends React.Component<{}> {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.text}>It works.</Text>
+        <Button text="neon-ui works!" />
       </View>
     )
   }
