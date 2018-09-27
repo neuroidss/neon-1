69be6b34811b5b155ea3941b6763e270f475b57b
Christopher David <chris@arcade.city>
2018-09-27 02:20:41 -0500

    Pull in sample react native component from ui2

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native'
-import { Button } from '@arcadecity/neon-ui'
+// import { Button } from '@arcadecity/neon-ui'   <Button text="neon-ui works!"
+import { Button } from '@arcadecity/neon-ui2'
 import test from '@arcadecity/neon-core/test'
 
 alert(test)
@@ -10,7 +11,7 @@ export default class App extends React.Component<{}> {
     return (
       <View style={styles.container}>
         <Text style={styles.text}>It works?</Text>
-        <Button text="neon-ui works!" />
+        <Button />
       </View>
     )
   }
@@ -19,7 +20,7 @@ export default class App extends React.Component<{}> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ ui2/button.js
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import { Text } from 'react-native'
+
+export function Button() {
+  return (
+    <Text>This is almost a button</Text>
+  )
+}
