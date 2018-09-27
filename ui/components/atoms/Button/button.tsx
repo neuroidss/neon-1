e8a634afda54416c6597d42dd88363f85548a182
Christopher David <chris@arcade.city>
2018-09-27 02:37:32 -0500

    works with typescript

--- ui/components/atoms/Button/button.tsx
+++ ui/components/atoms/Button/button.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react'
+import { ButtonProps } from './button.props'
 import { Text } from 'react-native'
 
-export function Button() {
+export function Button(props: ButtonProps) {
   return (
     <Text style={{color: 'white'}}>This is almost a button</Text>
   )
