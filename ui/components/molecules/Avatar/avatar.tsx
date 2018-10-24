3de03436de7fd20e9d6f4ee46072558bf6b131da
Nitish Kohade <nitishk@synerzipune.local>
2018-10-24 15:32:34 +0530

    code migration to neon for avatar component

--- ui/components/molecules/Avatar/avatar.tsx
+++ ui/components/molecules/Avatar/avatar.tsx
@@ -5,19 +5,45 @@ import { Container } from '../../atoms'
 import ArcadeCityBG from '../../../resources/images/city.jpg'
 import AvatarImg from '../../../resources/images/avatarImg.png'
 import { Button } from '../../atoms';
+import { Image, StyleSheet, View, TouchableOpacity } from 'react-native'
+
+const avatarStyle = StyleSheet.create({
+  base:  {
+    position: 'relative',
+    top: '-85px',
+    marginBottom: '-100px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
+
+const image = StyleSheet.create({
+  base: {
+    width: '150px',
+    height: '150px'
+  }
+})
+
+const selfie = StyleSheet.create({
+  base: {
+    position: 'absolute',
+    top: '60%'
+  }
+})
 
 export const Avatar = (props: any) => (
   <Container style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
-    <div>
-      <img width='100%' src={ArcadeCityBG} />
-      <div>
-        <img
-          src={!false ? AvatarImg : ''}
-        />
-        <div onClick={() => alert('yessss')}>
-          <Button />
-        </div>
-      </div>
-    </div>
+    <View style={avatarStyle.base}>
+      <Image style={image.base} source={{uri: ArcadeCityBG}} />
+      <View>
+        <Image source={{uri: AvatarImg}} />
+        <TouchableOpacity onPress={() => alert('yessss')}> 
+          <View style={selfie.base}>
+            <Button />
+          </View>
+        </TouchableOpacity>
+      </View>
+    </View>
   </Container>
 )
